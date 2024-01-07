48d670dd3f18b8acc4d53ba0830b5d4d146f82aa
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2024-01-07 19:54:20 -0300

    ternary condition

---
+++ ternario.js
@@ -0,0 +1,11 @@
+const minimunAge = 18;
+const clientAge = 16;
+
+if (clientAge >= minimunAge){
+    console.log("over 18 years old")
+} else{
+    console.log("under 18 years old")
+}
+
+              //condicion                 //true                 //false
+console.log(clientAge >= minimunAge ? "over 18 years old" : "under 18 years old")
