14b731888d71eee6cdc9a0452b65d8e5130f9ab5
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2024-01-07 20:45:59 -0300

    functions

---
+++ functionalities/funcitions.js
@@ -0,0 +1,20 @@
+let x = ""
+console.log(x);
+x = "hello"
+
+
+
+function printText(text) {
+    console.log(text)
+}
+
+printText("hello")
+
+function sum(n1, n2){
+    return n1 + n2;
+}
+
+console.log(sum(1,2))
+
+console.log(Math.sqrt(64)) //radical --> 8
+console.log(Math.random()) //randomic number between 1 and 0
