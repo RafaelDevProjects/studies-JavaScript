d7833988fe70f9f9c1d85f2b04d6f9e15b9c779c
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2024-01-07 22:54:20 -0300

    parameters

--- functionalities/funcitions.js
+++ functionalities/funcitions.js
@@ -16,5 +16,8 @@ function sum(n1, n2){
 
 console.log(sum(1,2))
 
+const sum1 = function(num1, num2) {return num1 + num2}
+console.log(sum1(1,5))
+
 console.log(Math.sqrt(64)) //radical --> 8
 console.log(Math.random()) //randomic number between 1 and 0
