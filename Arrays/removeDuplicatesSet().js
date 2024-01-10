9f0baf6c2a1736a1580dbe1ce3582f8c0fb17e0a
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2024-01-10 19:26:14 -0300

    Update removeDuplicatesSet().js

--- Arrays/removeDuplicatesSet().js
+++ Arrays/removeDuplicatesSet().js
@@ -1,2 +1,7 @@
-const nomes = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"];
+const names = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"];
 
+const mySet = new Set(names); // set
+
+const namesAtt = [...mySet]; // spread operator
+
+console.log(namesAtt)
