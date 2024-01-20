a8193a8cf8e1e196b3d7e9e38c2a9a20cafad1b3
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2024-01-20 18:25:25 -0300

    insertionSort

---
+++ Algorithms/insertionsort.js
@@ -0,0 +1,23 @@
+const books = require ('./list-books')
+
+function insertionSort(list) {
+    for (let actual = 0; actual < list.length; actual++){
+        let analise = actual;
+        while (analise > 0 && list[analise].price < list[analise-1].price){
+            let itemAnalise = list[analise];
+            let beforeItem = list[analise-1];
+
+            list[analise] = beforeItem
+            list[analise - 1] = itemAnalise
+ 
+            analise--
+        }
+
+    }
+
+
+    console.log(list)
+
+}
+
+insertionSort(books)
