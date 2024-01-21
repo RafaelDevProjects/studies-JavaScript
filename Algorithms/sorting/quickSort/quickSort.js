8c62f072ef5af643cd4d6382128aea3fb4afe66c
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2024-01-21 00:23:28 -0300

    Update quickSort.js

--- Algorithms/sorting/quickSort/quickSort.js
+++ Algorithms/sorting/quickSort/quickSort.js
@@ -5,21 +5,38 @@ function findSmaller(pivo, array){
 
     for (let actual= 0; actual < array.length; actual++){
         let actualObcject = array[actual]
-
         if (actualObcject.price < pivo.price){
             smallers++
         }
     }
 
-    changePlace(array, array.indexOf[pivo])
+    changePlace(array, array.indexOf(pivo), smallers)
+    return array
 }
 
 function changePlace(array, from, to){
     const elem1 = array[from]
     const elem2 = array[to]
-
     array[to] = elem1
     array[from] = elem2
 }
 
-console.log(findSmaller(listBooks[2], listBooks));
+function sliceInPivo(array){
+    let pivo = array[Math.floor(array.length / 2)];
+    findSmaller(pivo, array);
+    let valueSmallers = 0;
+
+    for(let analising = 0; analising < array.length; analising++){
+        let actual = array[analising];
+        if (actual.price <= pivo.price && actual !== pivo){
+            changePlace(array, analising, valueSmallers)
+            valueSmallers++;
+        }
+    }
+
+    return array;
+}
+
+//console.log(findSmaller(listBooks[2], listBooks));
+
+console.log(sliceInPivo(listBooks));
