06e3f7c65695eed4f7101deb8445749dce95b307
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2024-01-28 20:48:59 -0300

    libraries

---
+++ Libraries/Using_library/chalk.js
@@ -0,0 +1,4 @@
+import chalk from 'chalk'; // to change the way to import from requite to import ( import is the most up-to-date way to import) we need to add a this line in the packege.json '"type":"module"'
+
+
+console.log(chalk.blue('Hello world!'));
